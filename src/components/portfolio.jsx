1619a08c7c0c66689a345be6016feebce61199f8
Rohit <rohitranjan2702@gmail.com>
2023-03-03 10:40:35 +0530

    data.js added

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -10,15 +10,14 @@ const Project = () => {
     <header>
         <div class="container mx-auto py-4">
           <nav class="flex justify-between">
-            <a href="#" class="font-poppins font-semibold p-4 text-white text-[38px]">JD</a>
+            <a href="/" class="font-poppins font-semibold p-4 text-white text-[38px]">DEV</a>
             <ul class="flex mt-8 mr-8">
-                <li><a href="/" class="px-4 py-2 font-poppins text-greyy hover:text-white">Home</a></li>
-                <li><a href="/portfolio" class="px-4 py-2 font-poppins text-white hover:text-white">Portfolio</a></li>
+                <li><a href="/" class="px-4 py-2 font-poppins text-white hover:text-white" onClick={() => navigate("/")}>Home</a></li>
+                <li><a href="/portfolio" class="px-4 py-2 font-poppins text-greyy hover:text-white " onClick={() => navigate("/portfolio")}>Portfolio</a></li>
             </ul>
           </nav>
         </div>
       </header>
-
 
       <Skills/>
       <div class='grid grid-cols-1 sm:grid-cols-2 m-10'>
--- src/data.js
+++ src/data.js
@@ -1,28 +1,41 @@
+// Add about yourself here
 export const about = [
     {
-        name: "John Doe",
-        bio: "DevRel @ Google Inc",
-        about: " lorem ipsum dolor sit amet sap "
+        name: "Rohit Ranjan Singh",
+        gender:"male",              //use lowercase "male" or "female"
+        bio: "DevRel at ClueLess | Hacktoberfest’22 ",
+        about: "Passionate full-stack developer and UI designer. Love to explore new techs and make cool websites. "
     }
 ]
 
+
+
+// Add your G-drive pdf link here
+export const resumeLink = "http://localhost:56954/"
+
+// Dont change the order, add your social links here
 export const links = [
     {
         name: "github",
-        url:"#"
-    },
-    {
-        name: "linkedin",
-        url:"#"
-    },
-    {
-        name: "instagram",
-        url:"#"
+        url:"https://github.com/rohitranjan-2702"
     },
     {
         name: "twitter",
-        url:"#"
-    }
+        url:"https://twitter.com/rohit_ranjan27"
+    },
+    {
+        name: "linkedin",
+        url:"https://linkedin.com/in/rohit-ranjan-singh-6133901b6/"
+    },
+    {
+        name: "website",
+        url:"https://beacons.ai/rohitranjan"
+    },
+    {
+        name: "behance",
+        url:"https://fueler.io/rohitranjan"
+    },
+    
 ]
 
 export const skills = [
